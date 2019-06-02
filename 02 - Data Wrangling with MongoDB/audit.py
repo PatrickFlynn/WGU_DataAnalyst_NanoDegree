1aeab44536b4a44cc4127f9a63697f5d1c4b222c
PatrickFlynn <patrickseanflynn@gmail.com>
2019-06-02 15:21:19 -0700

    added map image, etc.

--- 02 - Data Wrangling with MongoDB/audit.py
+++ 02 - Data Wrangling with MongoDB/audit.py
@@ -11,7 +11,7 @@ filepath = r'C:\Users\p15144f\Desktop\WGU_DataAnalyst_NanoDegree\02 - Data Wrang
 osm_file = open(os.path.join(filepath, 'henderson.osm'),
                 "r", encoding="utf8")  
 
-#'addr:street'
+#Use regex to get street connector (i.e. ave, st, etc.)
 def return_street_connector(street_string):
     street_pattern = re.compile('\s(\w*$)')
     if re.findall(street_pattern, street_string):
@@ -19,12 +19,14 @@ def return_street_connector(street_string):
     else:
         return ''
 
+#Compare current street connector to expected list
 def street_expected_value(str_val):
     street_vals = ['Way', 'Drive', 'Avenue', 'Street', 'Road', 
                    'Lane', 'Parkway', 'Court', 'Highway', 'Trail', 
                    'Boulevard', 'Place', 'Cove']
     return any(str_val == s for s in street_vals)
 
+#Replace targeted errors seen during audit
 def replace_func(str_val):
     str_val = re.sub('\s\w{1}$', '', str_val)
     str_val = re.sub('Dr.', 'Drive', str_val)
@@ -35,34 +37,36 @@ def replace_func(str_val):
     str_val = re.sub('Ave\.', 'Avenue', str_val)
     return str_val
 
+#Ensure that zip code is 5 digits
 def audit_zip_code(postalcode):
     if not re.search('^\d{5}$', postalcode):
         return False
     else:
         return True
 
+#Ensure that coordinates follow Henderson standards
 def audit_lat_long(lat, long):
     if not re.search('\d{2}\.\d*', lat):
         return False
     elif not re.search('-\d{3}\.\d*', long):
         return False
     else:
-        return True
-    
-    
+        return True    
+
+
 def audit(file):
     tree = ET.parse(file)
     root = tree.getroot()
     for elem in root:
         if elem.tag == 'way':
             for tag in elem.iter("tag"):
-                
                 #Clean Streets
                 if tag.attrib['k'] == 'addr:street' and tag.attrib['v']:
                     if not street_expected_value(return_street_connector(tag.attrib['v'])):
                         print('Dirty Input:', tag.attrib['v'], '---> Sanitized Output:', replace_func(tag.attrib['v']))
                         tag.attrib['v'] = replace_func(tag.attrib['v'])
                 
+                #Ensure that county is always Clark, NV
                 if tag.attrib['k'] == 'tiger:county':
                     if tag.attrib['v'] != 'Clark, NV':
                         print('Error with County! --->', tag.attrib['v'])
@@ -71,17 +75,14 @@ def audit(file):
                 if tag.attrib['k'] == 'addr:postcode':
                     if not audit_zip_code(tag.attrib['v']):
                         print(tag.attrib['v'])
+                        
         #Check Lat Long to Ensure Integrity
         if elem.tag == 'node':
             if not audit_lat_long(elem.attrib['lat'], elem.attrib['lon']):
                 print(elem.attrib['lat'], elem.attrib['lon'])
+    #Write new XML file for use in analysis
     tree.write(os.path.join(filepath, 'Cleaned_Henderson.osm'))
 
 
 if __name__ == '__main__':
     audit(osm_file)
-    
-    
-    
-#tree = ET.parse(new_osm_file)
-#tree.write(r'C:\Users\p15144f\Desktop\WGU_DataAnalyst_NanoDegree\02 - Data Wrangling with MongoDB\test.osm')
